2c95111500b043f10f5c011f537ecc5cada2e84e
fraindly10 <alandadasheew@gmail.com>
2026-01-09 19:30:13 +0200

    commit

---
+++ 2.py
@@ -0,0 +1,35 @@
+'''
+#1
+num1, num2,num3, =1,2,3
+print(num1, num2, num3)
+num1, num2, num3 = 4,5,6
+print(num1, num2, num3)
+#2
+num1, num2, num3 =2,3,4
+print(num1, num2, num3)
+num1, num2, num3 = num3, num2, num1
+print(num1, num2, num3)
+#3
+num1, num2 = 7, 3
+total = num1 + num2
+print(total)
+#4
+num1,num2 = 15,4
+total = num1 - num2
+print(total)
+#5
+num1, num2 =6,5
+total = num1 * num2
+print(total)
+#6
+num1,num2 = 20,8
+total = num1 / num2
+print(total)
+'''
+#7
+num1 = int(input('введите первое число'))
+num2 =int(input('введите второе число'))
+print((num1+num2)/2)
+# total =(num1+num2)/2
+# print(total)
+
