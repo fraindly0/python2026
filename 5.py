7b86babe7b8fdac038ca50166e00e869c472ee47
fraindly10 <alandadasheew@gmail.com>
2026-01-27 19:34:07 +0200

    5

---
+++ 5.py
@@ -0,0 +1,60 @@
+# ? составные вырожения / (and, or)
+
+
+# * И (and)
+age = 25
+balance = 1000
+if age >= 18 and balance > 500:
+    print("покупка разрешена")
+else:
+    print("Нелостаточно средств или мал возраст")
+
+# * ИЛИ (or)
+day = "sunday"
+if day == "sunday" or day == "saturday":
+    print("Выходной")
+else:
+    print("сегодня выходной")
+
+# * НЕ (not)
+is_danned = False
+if not is_danned:
+    print("Доступ рзрешено")
+else:
+    print("Доступ разрешон")
+
+# ! порядок выполнения действий
+# * ()
+# * not
+# * and
+# * or
+
+# ? вложенные конструкции
+username = "fraindly10"
+password = "1234567890"
+if username == "fraindly10":
+    print("логин пороль")
+    if password == "1234567890":
+        print("всё верно")
+    else:
+        print("Пороль не верный")
+else:
+    print("Логин не верный")
+
+#1
+# time=int(input("видите время"))
+# if time>9 and time<21:
+#     print("магазин открыт")
+# else:
+#     print("магазин закрыт")
+
+#2
+diskontnaikarta=input("есть ли карта постоянного посетителя ")
+caunt=int(input("ведите на  сколько денег вы потратили"))
+if diskontnaikarta == "yes" and caunt>5000:
+    print("вы получаете скидку")
+else:
+    print("нет не дам скидку")
+
+
+
