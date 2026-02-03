46992d0d6d99a011a33f73357b41b29ff20123a3
fraindly10 <alandadasheew@gmail.com>
2026-02-03 19:30:20 +0200

    5

---
+++ 6.py
@@ -0,0 +1,32 @@
+# # ? цикл for
+# # * 1 перебор чисел range
+# for i in range(10): # 1 2 3 4 5
+#     print(f"Интопритация: {i}")
+#
+# # * 2 /5 do
+# for i in range(5,10):  #5 6 7 8 9
+#     print(f"Интопритация: {i}")
+#
+# # * 3 от | до
+# for i in range(5, 10,2): # i = 5 7 9
+#     print(f"Итерация: {i}")
+
+# #1
+# for i in range(10,0,-1):
+#     print(f'поехали: {i}')
+
+
+# a=int(input())
+# for i in range(11):
+#     print(f'{i}*{a}={i*a}')
+
+# a=int(input())
+# # for i in range(0,a+1,2):
+# #     print(i)
+
+
+# a=int(input())
+# total=0
+# for i in range(0,a+1):
+#     total+=i
+# print(total)
