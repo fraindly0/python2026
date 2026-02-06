3f6f9fa44bc23bc45e329c4e4fe333429b58947a
fraindly10 <alandadasheew@gmail.com>
2026-02-06 19:29:55 +0200

    5

---
+++ 16.1.py
@@ -0,0 +1,94 @@
+# n=int(input()) # ? количество чисел
+# total=0
+# for i in range(n):
+#     a = int(input())
+#     if a%6==0:
+#         total +=a
+# print(total)
+
+# n=int(input())
+# total=0
+# for i in range(n):
+#     a = int(input())
+#     if a%4==0:
+#         total +=a
+# print(total)
+
+# n = int(input())
+# total = 0
+# for i in range(n):
+#     a = int(input())
+#     if  a%6==0 and a%10==4:
+#         total += a
+# print(total)
+
+
+# n=int(input())
+# m=0
+# for i in range(n):
+#     a=int(input())
+#     if a%5==0 and a>m:
+#         m=a
+# print(m)
+
+# n=int(input())
+# day=0
+# total=0
+# for i in range(n):
+#     a=int(input())
+#     if a>10:
+#         day+=1
+#         total+=a
+# print(total/day)
+# print(day)
+
+# n=int(input())
+# total=0
+# b=0
+# for i in range(n):
+#     a=int(input())
+#     if a>13 and a<20:
+#         b+=1
+#         total+=a
+# print(total/b)
+# print(b)
+
+# n=int(input())
+# total=0
+# g=0
+# for i in range(n):
+#     a=int(input())
+#     if a>10 and a<15:
+#         total+=a
+#         g+=1
+# print(total/g)
+# print(g)
+
+
+# n=int(input())
+# total=0
+# for i in range(n):
+#     a=int(input())
+#     if a<30 and a%2==0:
+#         total+=a
+# print(total)
+
+# n=int(input())
+# total=0
+# for i in range(n):
+#     a=int(input())
+#     if a<25 and not a%10==3:
+#         total+=a
+# print(total)
+
+# n=int(input())
+# total=0
+# for i in range(n):
+#     a=int(input())
+#     if a%221<7:
+#         total+=1
+# print(total)
+
+
+
+
