a98071c1130a58d36b5dc980e323a38f44ebccca
fraindly10 <alandadasheew@gmail.com>
2026-02-20 19:31:35 +0200

    67

---
+++ 10_16_55.py
@@ -0,0 +1,99 @@
+# Напишите программу подсчёта суммы цифр в записи натурального числа.
+# На вход программе подаётся натуральное число. Программа должна напечатать только одно число - сумму цифр
+
+# n=int(input())
+# s = 0
+# while n > 0:
+#     k = n % 10
+#     s +=k
+#     n = n // 10
+# print(s)
+
+#1
+# n = int(input())
+# w = 0
+# while n > 0:
+#     k = n % 10
+#     if k % 3 == 0:
+#         w += 1
+#     n =  n // 10
+# print(w)
+
+#2
+# n = int(input())
+# w = 0
+# while n > 0:
+#     k = n % 10
+#     if k % 4 == 0:
+#         w += 1
+#     n =  n // 10
+# print(w)
+
+#3
+# n = int(input())
+# w =0
+# while n != 0:
+#     n = int(input())
+#     if n % 10 == 4 and n % 6 ==0:
+#         w += n
+# print(w)
+
+#4
+# w = 0
+# n = int(input())
+# while n != 0:
+#     if n % 4 == 0 or n % 9 == 0:
+#         w += n
+#     n = int(input())
+# print(w)
+
+#5
+# n = int(input())
+# w=0
+# while n != 0:
+#     k = n % 10
+#     if k % 4 != 0:
+#         w += k
+#     n = n // 10
+# print(w)
+
+#6
+# n = int(input())
+# w=0
+# while n != 0:
+#     k= n % 10
+#     if k % 3 != 0:
+#         w += k
+#     n = n // 10
+# print(w)
+
+#7
+# n = int(input())
+# w = 0
+# while n != 0:
+#     k = n % 10
+#     if k>7:
+#         w +=k
+#     n = n // 10
+# print(w)
+
+#8
+# n = int(input())
+# w = 0
+# while n != 0:
+#     if n % 4 == 0:
+#         w +=n
+#     n = int(input())
+# print(w)
+
+#9
+# n = int(input())
+# w = 0
+# while n != 0 :
+#     k = n % 10
+#     if k<6 :
+#         w += k
+#     n = n //10
+# print(w)
+
+
---
+++ 16_4.py
@@ -0,0 +1,88 @@
+# count = int(input())
+# num_sum = 0
+# for i in range(count):
+#     num = int(input())
+#     if num % 11 == 4:
+#         num_sum += num
+# print(num_sum)
+
+
+# count = int(input())
+# num_sum = 0
+# for i in range(count):
+#     num = int(input())
+#     if num % 25 == 6:
+#         num_sum += num
+# print(num_sum)
+
+# count = int(input())
+# num = 0
+# for i in range(count):
+#     num = int(input())
+#     if num % 27 == 16:
+#         num += num
+# print(num)
+
+# sss=int(input())
+# # ssss=0
+# # for i in range(sss):
+# #     ssss=int(input())
+# #     if ssss % 81 == 14:
+# #         ssss += ssss
+# # print(ssss)
+
+# pamidor=int(input())
+# pppp=0
+# for i in range(pamidor):
+#     agurew=int(input())
+#     if agurew % 12 == 7:
+#         pppp += agurew
+# print(pppp)
+
+# p=int(input())
+# w=0
+# for i in range(p):
+#     a=int(input())
+#     if a % 64 == 26:
+#         w+=a
+# print(w)
+
+
+# p=int(input())
+# w=0
+# for i in range(p):
+#     a=int(input())
+#     if a % 49 == 15:
+#         w+=a
+# print(w)
+
+
+# p=int(input())
+# w=0
+# d=0
+# for i in range(p):
+#      a=int(input())
+#      if a % 7  == 5:
+#          w+=a
+#          d+=1
+# print(w//d)
+
+# type=int(input())
+# w=0
+# for i in range(type):
+#     a=int(input())
+#     if a % 7 == 1:
+#         w+=a
+# print(w)
+
+# o=int(input())
+# w=0
+# for i in range(o):
+#     a=int(input())
+#     if a % 12 == 7:
+#         w+=a
+# print(w)
+
+
+
+
